fbaa907dbcaf0c45b6ba3818cccaa9597877c6f2
bot-maker <none>
2017-03-28 19:07:06 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -3,14 +3,14 @@ newScript()
     response.sendText('How can I help you?');
   })
   .expect
-    .intent('business', 'exchange', (session, response) =>{
-      response.sendText('I can start the exchange process now');
+    .intent('business', 'exchange', (session, response) => {
+      response.sendText('You\'ll need to email us at help@shop.com to do a return');
     })
-    .intent('business', 'exchange', (session, response) =>{
+    .intent('business', 'return procedure', (session, response) => {
       response.sendText('I can start the exchange process now');
     })
     .catch((session, response) => {
       response.sendText('I don\'t understand, can you try again?');
-      console.log(session.intent);
+      console.log('unknown message', session.intent, session.message);
     })
     
