7da7f10da4754fc96a14a29678b6ae7f14d10b02
bot-maker <none>
2017-03-28 19:20:07 +0000

    Saved with alana.cloud

--- scripts/return.js
+++ scripts/return.js
@@ -1,6 +1,30 @@
 // Created Tue Mar 28 2017
+
 newScript('return')
-  // to do, 
   .dialog((session, response) => {
-    response.sendText('What is the order number you')
+    response.sendText('What is the order number?')
   })
+  // to do, send buttons with last 3 orders of user
+  // taken from api
+  .expect
+    .text((session, response) => {
+      const order = session.message.text;
+      response.sendText(`I found order ${order} 👍`);
+      response.sendText('Print the label, put label on the box, ship the box, get 💰')
+      response.sendButtons()
+        .addButton('url', 'Get label', 'http://google.com')
+        .send()
+      /*
+      request({
+        uri: 'https://api.shop.com/getorder',
+        method: 'POST'
+        json: true,
+        body: {
+          id: order,
+        },
+      })
+        .then((response) => {
+          response.sendText('sen)
+        })
+      */
+    })
