ab87f52f0ca24f01eb38b5ef54961c8f1385754f
bot-maker <none>
2017-03-28 19:36:04 +0000

    Saved with alana.cloud

--- tests/main.js
+++ tests/main.js
@@ -13,5 +13,15 @@ test('returns', function() {
     .sendText('need to return my last order')
     .expectText('I can start the exchange process now')
     .expectText('What is the order number?')
+    .sendText('12345')
+    .expectText('I found order 12345 👍')
+    .expectText('Print the label, put label on the box, ship the box, get 💰')
+    .expectButtons('', [
+        {
+            type: 'url',
+            text: 'Get label',
+            url: 'http://google.com'
+        },
+    ])
     .run();
 })
