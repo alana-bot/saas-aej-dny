c21872a940c726d63fa64dbf0a9fe6080c20fea3
bot-maker <none>
2017-03-28 19:43:03 +0000

    Saved with alana.cloud

--- tests/exchange.js
+++ tests/exchange.js
@@ -1,1 +1,11 @@
 // Created Tue Mar 28 2017
+
+test('greeting', function() {
+  return newTest('adam')
+    .expectText('Welcome to our online store')
+    .expectText('How can I help you?')
+    .sendText('I need to exchange my dress for a smaller size')
+    .expectText('You\'ll need to email us at help@shop.com to do a return')
+    .expectText('How else can I help you?')
+    .run();
+})
