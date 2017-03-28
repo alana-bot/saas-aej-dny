605f76644b3af15ac8d70116feb5b884f5cd0b88
bot-maker <none>
2017-03-28 19:44:55 +0000

    Saved with alana.cloud

--- tests/exchange.js
+++ tests/exchange.js
@@ -6,7 +6,5 @@ test('exchange', function() {
     .expectText('Welcome to our online store')
     .expectText('How can I help you?')
     .sendText('I need to exchange my dress for a smaller size')
-    .expectText('You\'ll need to email us at help@shop.com to do a return')
-    .expectText('How else can I help you?')
     .run();
 })
