6ba7cabec9c151b7bfd71d874f22d3ceadd0b080
bot-maker <none>
2017-03-28 18:40:09 +0000

    Saved with alana.cloud

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -2,5 +2,4 @@
 
 addGreeting(function(user, response){
     response.sendText('Welcome to our online store');
-    response.sendText('How can I help you?');
 });
--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
 newScript()
   .dialog((sessions, response) => {
-    response.sendText('hello world');
-  });
+    response.sendText('How can I help you?');
+  })
+  .expect
+    .intent('business', 'exchange', (session, response) =>{
+      response.sendText('I can start the exchange process now');
+    })
+    .intent('business', 'exchange', (session, response) =>{
+      response.sendText('I can start the exchange process now');
+    })
+    .catch((session, response) => {
+
+    })
+    
