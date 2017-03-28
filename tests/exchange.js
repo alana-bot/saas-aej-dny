081391ad6e6495bc28bd3a76e096132ac592754b
bot-maker <none>
2017-03-28 19:44:26 +0000

    Saved with alana.cloud

--- tests/exchange.js
+++ tests/exchange.js
@@ -2,6 +2,7 @@
 
 test('greeting', function() {
   return newTest()
+    .checkForTrailingDialogs(true)
     .expectText('Welcome to our online store')
     .expectText('How can I help you?')
     .sendText('I need to exchange my dress for a smaller size')
