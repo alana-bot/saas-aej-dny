b3e19efb3b3f8f595a813a14e2bfa25805c0256e
bot-maker <none>
2017-03-29 20:17:47 +0000

    Saved with alana.cloud

--- scripts/main.js
+++ scripts/main.js
@@ -9,6 +9,7 @@ newScript()
   .expect
     .intent('business', 'exchange', (session, response) => {
       response.sendText('You\'ll need to email us at help@shop.com to do a return');
+      response.goto('start');
     })
     .intent('business', 'return procedure', (session, response) => {
       response.sendText('I can start the exchange process now');
